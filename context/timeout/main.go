597bd70389aef87d813c35e6bc023d7c0727b6b6
ejunjsh <sjj050121014@163.com>
2018-08-16 15:14:06 +0800

    update "context"  related code

---
+++ context/timeout/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "context"
+    "log"
+    "os"
+    "time"
+)
+
+var logg *log.Logger
+
+func someHandler() {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    go doStuff(ctx)
+
+//10秒后取消doStuff
+    time.Sleep(10 * time.Second)
+    cancel()
+}
+
+//每1秒work一下，同时会判断ctx是否被取消了，如果是就退出
+func doStuff(ctx context.Context) {
+    for {
+        time.Sleep(1 * time.Second)
+        select {
+        case <-ctx.Done():
+            logg.Printf("done")
+            return
+        default:
+            logg.Printf("work")
+        }
+    }
+}
+
+func main() {
+    logg = log.New(os.Stdout, "", log.Ltime)
+    someHandler()
+    logg.Printf("end")
+}
