bcfb003f3397235352093792eb44f5bbb896de52
zhouff <sjj050121014@163.com>
2018-08-18 22:49:31 +0800

    update some code about default value

---
+++ nil/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a int
+	var b bool
+	var g string
+	var c func()
+	var d []int
+	var e map[string]int
+	var f [10]string
+	var h interface{}
+
+
+	fmt.Println(a,b,c,d,e,f,g,h)
+}
