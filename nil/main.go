14848290f777fc28a9c527fd6044e83934837bb3
zhouff <sjj050121014@163.com>
2018-08-19 14:52:28 +0800

    minor change

--- nil/main.go
+++ nil/main.go
@@ -11,7 +11,13 @@ func main()  {
 	var e map[string]int
 	var f [10]string
 	var h interface{}
+	var i error
+	var j chan int
 
-
-	fmt.Println(a,b,c,d,e,f,g,h)
+	fmt.Println(a,b,c,d,e,f,g,h,j)
+	fmt.Println(d==nil)
+	fmt.Println(e==nil)
+	fmt.Println(h==nil)
+	fmt.Println(i==nil)
+	fmt.Println(j==nil)
 }
